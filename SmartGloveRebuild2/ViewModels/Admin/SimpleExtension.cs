076498c05925c70d04533d7a939de53645e9c4b9
LOO WEI LER <looweiler20010916@gmail.com>
2023-02-23 11:10:14 +0800

    Added Signature for HR, BU Head report pdf, and also added some simple extra extension, empty view for collectionview from AssignGroup to Display Group page

--- SmartGloveRebuild2/ViewModels/Admin/SimpleExtension.cs
+++ SmartGloveRebuild2/ViewModels/Admin/SimpleExtension.cs
@@ -30,6 +30,8 @@ namespace SmartGloveRebuild2.ViewModels.Admin
         // displays only text label
         public static void LabelCell(this IContainer container, string text) => container.Cell(true).Text(text).Medium();
         public static void EmptyCell(this IContainer container, string text) => container.OriginalCell().AlignMiddle().Text(text).FontSize(10);
+        public static void SignatureSubscriptCell(this IContainer container, string text) => container.OriginalCell().BorderTop(1).AlignBottom().Text(text).FontSize(6);
+        public static void SignatureEmptyCell(this IContainer container, string text) => container.OriginalCell().BorderBottom(1).AlignBottom().Text(text).FontSize(6);
         public static void BoldEmptyCell(this IContainer container, string text) => container.OriginalCell().AlignMiddle().Text(text).FontSize(10).ExtraBold();
         public static void HeaderEmptyCell(this IContainer container, string text) => container.OriginalCell().PaddingVertical(10).AlignMiddle().Text(text).FontSize(18);
         public static void CheckBox(this IContainer container) => container.OriginalCell().Border(1).Padding(0).Text("");
